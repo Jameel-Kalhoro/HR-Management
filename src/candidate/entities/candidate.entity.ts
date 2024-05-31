db689d8a6a2f16415be211125674180c8fc8a079
unknown <jameel.bscsf19@iba-suk.edu.pk>
2024-05-31 04:46:04 -0700

    candidate entity added

--- src/candidate/entities/candidate.entity.ts
+++ src/candidate/entities/candidate.entity.ts
@@ -1,1 +1,19 @@
-export class Candidate {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Candidate {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    resume: string;
+
+    @Column()
+    status: string;
+}
